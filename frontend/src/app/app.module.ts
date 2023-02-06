bf7e36c922b88015be617119b08901b0e4f6facf
Christian Dean <christiandeandemesa@gmail.com>
2023-02-06 15:55:19 -0800

    Not Found Message

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,10 +11,11 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
   // declarations holds the components we can use in our app.
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent, TitleComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent, TitleComponent, NotFoundComponent],
   // imports holds the libraries/frameworks we can use in our app.
   imports: [BrowserModule, AppRoutingModule, RatingModule],
   providers: [],
